43bd0f5bb9ba103ec1c5fe5d10d6abc51093011a
Ivan Milankovic <imilankovic@maxeler.com>
2015-04-24 19:01:17 +0200

    Updated directory structure, Makefile and README files.

---
+++ APP/dfe/RunRules/Simulation/objects/main.d
@@ -0,0 +1,82 @@
+../RunRules/Simulation/objects/main.o: main.c \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_private/slic_private_type.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_typedefs.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_private/slic_private_typedefs.h \
+ ../RunRules/Simulation/include/BrainNetwork.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/MaxSLiCInterface.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_engine.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_typedefs.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_engarray.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_group.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_run.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_maxfile.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_actions.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_actarray.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_configuration.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_scalar.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_mem.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_stream.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_error.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_error_list.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_compiler.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_event_monitoring.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_topology.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_framed_streams.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_llstream.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_network.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_tcp.h \
+ /network-raid/opt/maxcompiler-2014.2/include/slic/slic_udp.h
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_private/slic_private_type.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_typedefs.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_private/slic_private_typedefs.h:
+
+../RunRules/Simulation/include/BrainNetwork.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/MaxSLiCInterface.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_engine.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_typedefs.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_engarray.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_group.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_run.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_maxfile.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_actions.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_actarray.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_configuration.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_scalar.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_mem.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_stream.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_error.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_error_list.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_compiler.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_event_monitoring.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_topology.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_framed_streams.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_llstream.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_network.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_tcp.h:
+
+/network-raid/opt/maxcompiler-2014.2/include/slic/slic_udp.h:
